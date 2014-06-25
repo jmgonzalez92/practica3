0a959ec69ccd64fcf6ec7530e673676156dc4a66
jmgonzalez92 <jmgonzalez92@hotmailes>
2014-06-25 10:06:08 +0200

    eliminarpersona

--- practica03/eliminarpersona.aspx.cs
+++ practica03/eliminarpersona.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using practica03.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,26 @@ namespace practica03
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                PersonManager proxy = new PersonManager();
+
+                this.DropDownList2.DataSource = proxy.GetAll();
+                this.DropDownList2.DataTextField = "LastName";
+                this.DropDownList2.DataValueField = "id";
+                this.DropDownList2.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(this.DropDownList2.SelectedValue);
+
+            PersonManager proxy = new PersonManager();
+
+            proxy.Remove(id);
+
+
 
         }
     }
